d9cffb7b9f85a191c881b44be54e7b6208dd6624
murillojoaquim <murillojalves@hotmail.com>
2021-08-20 20:46:35 -0300

    Contagem

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Preparado?\n");
+            Console.ReadKey();
+
+            Console.ResetColor();
+            
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Preparar...\n");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Apontar...\n");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("FOGO!\n");
+            Console.ReadKey();
+
+            Console.ResetColor();
+
+            Console.WriteLine("                                                                                                                                                                       ");
+            Console.WriteLine("                        _                                                                                                                                              ");
+            Console.WriteLine("                       | `-._                                                                                                                                          ");
+            Console.WriteLine("     __________________|_____`._______          ____________________________.-.____________                                                                            ");
+            Console.WriteLine(" __.`-------------------____----------`--/`._.`-----|                        o|            |         não sei fazer uma bala saindo, então finge que tem uma bala aqui  ");
+            Console.WriteLine("||                     (____)            |          | ======================  |____________|         BARULHO DE BALA POW POW '-'                                       ");
+            Console.WriteLine(" ||_______________________________________|__________| ======================  |                                                                                       ");
+            Console.WriteLine("'-._o                          o____(_)_____________| ============= ____..o---'                                                                                        ");
+            Console.WriteLine("    `.           __0-------''-`          `---..--._(|_o______.-----`                                                                                                   ");
+            Console.WriteLine("      )     o  .' //       ||                                                                                                                                          ");
+            Console.WriteLine("     /        (             ||                                                                                                                                         ");
+            Console.WriteLine("    /           ___________//                                                                                                                                          ");
+            Console.WriteLine("   /         __/-----------`                                                                                                                                           ");
+            Console.WriteLine("_./         (                                                                                                                                                          ");
+            Console.WriteLine("-------------                                                                                                                                                          ");
+
+        }
+    }
+}
